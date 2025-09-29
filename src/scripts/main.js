412fbc41672a3c1831de9f8136d3a5ae2792cab3
Dmytro Lymar <dmytrolymar901@gmail.com>
2025-09-29 17:09:53 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const populations = [...document.querySelectorAll('span.population')].map(
-  (el) => Number.parseInt(el.textContent.replaceAll(',', '')),
+  (el) => {
+    const text = el.textContent || '';
+    const cleared = text.replace(/[^\d-]/g, '');
+    const num = Number(cleared);
+
+    if (Number.isFinite(num)) {
+      return num;
+    }
+  },
 );
 
-const totalElement = document.querySelector('span.total-population');
-const avgElement = document.querySelector('span.average-population');
+if (populations.length > 0) {
+  const total = populations.reduce((acc, num) => acc + num, 0);
+  const avg = Math.round(total / populations.length);
 
-const total = populations.reduce((acc, num) => acc + num, 0);
-const avg = Math.round(total / populations.length);
+  const totalElement = document.querySelector('span.total-population');
+  const avgElement = document.querySelector('span.average-population');
 
-totalElement.textContent = total.toLocaleString('en-US');
-avgElement.textContent = avg.toLocaleString('en-US');
+  totalElement.textContent = total.toLocaleString('en-US');
+  avgElement.textContent = avg.toLocaleString('en-US');
+}
